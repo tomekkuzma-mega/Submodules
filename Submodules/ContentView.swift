5b2c42978cfca6df303bd7b7c9f60509d2f4f476
Tomasz Kuźma <tku@mega.co.nz>
2023-11-17 19:21:36 +0100

    Initial Commit

---
+++ Submodules/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Submodules
+//
+//  Created by mega on 17/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Submodules/SubmodulesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubmodulesApp.swift
+//  Submodules
+//
+//  Created by mega on 17/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SubmodulesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
